b0d2074d10acdfa2a2e7bc53c413a0cc6c4b1835
miguelsepul <miguel.sepulveda@utp.edu.co>
2025-12-02 10:36:35 -0500

    Direccionamiento IP de VM

--- launchMED.ps1
+++ launchMED.ps1
@@ -4,7 +4,7 @@
 Start-Process powershell -ArgumentList "-NoExit", "-Command", ".\distributedKV.exe --db-location=Medellin.db --http-addr=10.1.0.4:8080 --config-file=sharding-distributed.toml --shard=Medellin"
 
 # Iniciar réplica
-# Start-Process powershell -ArgumentList "-NoExit", "-Command", ".\distributedKV.exe --db-location=Medellin-r.db --http-addr=127.0.0.33:8080 --config-file=sharding.toml --shard=Medellin --replica"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", ".\distributedKV.exe --db-location=Medellin-r.db --http-addr=10.1.0.4:8081 --config-file=sharding-distributed.toml --shard=Medellin --replica"
 
 Write-Host "Se ha iniciado el nodo MEDELLIN y su réplica."
 Write-Host "1 nodo principal + 1 replica = 2 procesos en total"
--- launchPEI.ps1
+++ launchPEI.ps1
@@ -4,7 +4,7 @@
 Start-Process powershell -ArgumentList "-NoExit", "-Command", ".\distributedKV.exe --db-location=Pereira.db --http-addr=10.1.2.4:8080 --config-file=sharding-distributed.toml --shard=Pereira"
 
 # Iniciar réplicas
-# Start-Process powershell -ArgumentList "-NoExit", "-Command", ".\distributedKV.exe --db-location=Pereira-r.db --http-addr=127.0.0.22:8080 --config-file=sharding.toml --shard=Pereira --replica"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", ".\distributedKV.exe --db-location=Pereira-r.db --http-addr=10.1.2.4:8081 --config-file=sharding-distributed.toml --shard=Pereira --replica"
 
 Write-Host "Se ha iniciado el nodo PEREIRA y su réplica."
 Write-Host "1 nodo principal + 1 replica = 2 procesos en total"
